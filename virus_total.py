34ac16447913f4fbeb4eada87d6c0c4773e4de7c
Stephen Tate <accounts@sjtate.co.uk>
2016-09-09 11:42:10 +0100

    Dump Output.
    Added the functionality to dump the full JSON of the report.
    Added the functionality to output a csv file of the report. Only adds certain fields to the the output.

--- virus_total.py
+++ virus_total.py
@@ -1,13 +1,11 @@
-import ConfigParser
+import configparser
 import requests
 import json
+import csv
+
 
 class VirusTotal(object):
-    def __init__(self):
-        # Define the configuration file
-        config = ConfigParser.ConfigParser()
-        config.read('conf.ini')
-        
+    def __init__(self):        
         # Get the API Key from the config file
         self.apikey = config.get('Remote_Configuration', 'API_Key')
 
@@ -37,9 +35,41 @@ class VirusTotal(object):
         result = requests.post(url, data=params)
 
         if result.status_code == self.HTTP_OK:
-            print(json.loads(result.text))
+            result = json.loads(result.text)
+            return result
 
 
+def dump_to_jsonfile(json_data):
+    filepath = config.get('Output_Files', 'Output_JSON')
+    with open(filepath, mode='a') as output_file: 
+        json.dump(json_data, output_file, indent=4, sort_keys=True)
 
+def write_to_csv(json_data):
+    # Get the useful fields
+    url = json_data['url']
+    scandate = json_data['scan_date']
+    positives = json_data['positives']
+    total_engines = json_data['total']
+    selected_engine = vt.network_detection_engine_preference
+    selected_detected = json_data['scans'][vt.network_detection_engine_preference]['detected']
+    selected_result = json_data['scans'][vt.network_detection_engine_preference]['result']
+
+    # build csv row
+    row = [url, scandate, positives, total_engines, selected_engine, selected_detected, selected_result]
+
+    # CSV to write to 
+    filepath = config.get('Output_Files', 'Output_CSV')
+    with open(filepath, mode='a') as output_file:
+        csv_writer = csv.writer(output_file)
+        csv_writer.writerow(row)  
+
+
+# Define the configuration file
+config = configparser.ConfigParser()
+config.read('conf.ini')
+
+# Establish VT 
 vt = VirusTotal()
-vt.retrieve_domain_report('http://google.com')
+report = vt.retrieve_domain_report('http://google.com')
+dump_to_jsonfile(report)
+write_to_csv(report)
